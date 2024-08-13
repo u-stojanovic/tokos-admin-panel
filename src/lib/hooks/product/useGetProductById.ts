15df92d09e08b83ca4d7f22658c403b33d126d1f
Boris Nikolic <borisnikolic2302@gmail.com>
2024-08-13 16:43:47 +0200

    feat: Skeleton Loader for unique product page

--- src/lib/hooks/product/useGetProductById.ts
+++ src/lib/hooks/product/useGetProductById.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductById } from "../../actions/productActions";
+import { Product } from "@prisma/client";
 
 const getProduct = async (id: number) => {
-  const users = await getProductById(id);
-  return users;
+  const product = await getProductById(id);
+  return product;
 };
 
 export const useGetProductById = (id: number) => {
-  return useQuery({
+  return useQuery<Product | null>({
     queryKey: ["getProduct", id],
     queryFn: ({ queryKey }) => getProduct(queryKey[1] as number),
   });
