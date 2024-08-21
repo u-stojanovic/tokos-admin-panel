186a64778f0607c2de87fb44759fe4c3eb1ee5d3
Boris Nikolic <borisnikolic2302@gmail.com>
2024-08-21 11:18:18 +0200

    refetching orders after new order creation from official website

--- src/lib/newOrderProvider.tsx
+++ src/lib/newOrderProvider.tsx
@@ -3,6 +3,7 @@
 import React, { createContext, useContext, ReactNode } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { useWebSocket } from "@/lib/hooks/useWebSocket";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface NewOrderContextType {}
 
@@ -12,11 +13,19 @@ const NewOrderContext = createContext<NewOrderContextType | undefined>(
 
 export const NewOrderProvider = ({ children }: { children: ReactNode }) => {
   const { toast } = useToast();
+  const queryClient = useQueryClient();
 
   const handleNewOrder = () => {
     toast({
+      className:
+        "top-0 right-0 flex fixed md:max-w-[420px] md:top-4 md:right-4",
       title: "New Order",
       description: "A new order has been placed.",
+      variant: "destructive",
+    });
+    queryClient.invalidateQueries({
+      queryKey: ["orders"],
+      refetchType: "all",
     });
   };
 
