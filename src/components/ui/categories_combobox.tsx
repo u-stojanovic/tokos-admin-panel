5dd4135faf67c6dbb475c28a612eeddd21a3947a
Boris Nikolic <borisnikolic2302@gmail.com>
2024-08-22 11:33:36 +0200

    fix: combobox categories

--- src/components/ui/categories_combobox.tsx
+++ src/components/ui/categories_combobox.tsx
@@ -17,21 +17,13 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Input } from "@/components/ui/input";
 import { Category } from "@prisma/client";
 import { getCategories } from "@/lib/actions/categoriesActions";
-import { z } from "zod";
-import { useToast } from "@/components/ui/use-toast";
 
 interface CategoriesComboBoxProps {
   currCategory?: string | undefined;
   onChange: (category: string) => void;
 }
-
-const categorySchema = z
-  .string()
-  .min(1, "Category name is required")
-  .max(50, "Category name must be less than 50 characters");
 
 export function CategoriesComboBox({
   currCategory,
@@ -40,8 +32,6 @@ export function CategoriesComboBox({
   const [open, setOpen] = React.useState(false);
   const [categories, setCategories] = React.useState<Category[]>([]);
   const [selectedCategory, setSelectedCategory] = React.useState(currCategory);
-  const [newCategory, setNewCategory] = React.useState("");
-  const { toast } = useToast();
 
   React.useEffect(() => {
     async function fetchCategories() {
@@ -58,31 +48,6 @@ export function CategoriesComboBox({
       setSelectedCategory(currentValue);
       onChange(currentValue);
       setOpen(false);
-    }
-  };
-
-  const handleNewCategoryInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    const value = event.target.value;
-    setNewCategory(value);
-  };
-
-  const handleAddNewCategory = () => {
-    try {
-      categorySchema.parse(newCategory);
-      setSelectedCategory(newCategory);
-      onChange(newCategory);
-      setOpen(false);
-    } catch (e) {
-      if (e instanceof z.ZodError) {
-        e.errors.forEach((error) => {
-          toast({
-            title: "Validation Error",
-            description: error.message,
-          });
-        });
-      }
     }
   };
 
@@ -124,37 +89,8 @@ export function CategoriesComboBox({
                   </CommandItem>
                 ))}
             </CommandGroup>
-            <CommandItem
-              key="new-category"
-              value="new-category"
-              onSelect={() => handleSelect("new-category")}
-            >
-              <Check
-                className={cn(
-                  "mr-2 h-4 w-4",
-                  selectedCategory === "new-category"
-                    ? "opacity-100"
-                    : "opacity-0",
-                )}
-              />
-              {`Create new category`}
-            </CommandItem>
           </CommandList>
         </Command>
-        {selectedCategory === "" && (
-          <div className="p-4 flex flex-col gap-2">
-            <Input
-              type="text"
-              value={newCategory}
-              onChange={handleNewCategoryInputChange}
-              placeholder="Enter new category"
-              className="w-full p-2 border rounded"
-            />
-            <Button onClick={handleAddNewCategory} size="default">
-              Add new Category
-            </Button>
-          </div>
-        )}
       </PopoverContent>
     </Popover>
   );
